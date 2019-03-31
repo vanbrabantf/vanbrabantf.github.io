76730800fe1418e83a7d7a91c946f4c89117bf34
Frederick Vanbrabant <frederick@brodyandhooper.com>
2019-03-31 16:12:31 +0200

    Big cleanup

--- static/scripts/anchors.js
+++
@@ -1,7 +0,0 @@
-Array.from(document.querySelectorAll('h2, h3, h4, h5, h6'))
-    .filter(function (heading) {
-        return heading.id;
-    })
-    .map(function (heading) {
-        heading.innerHTML = '<a href="#' + heading.id + '">' + heading.innerHTML + '</a>';
-    });
